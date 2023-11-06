d9ced6bed6ca0ae385cbc87ddea640ed58924641
ManuAsta <107803795+ManuAsta@users.noreply.github.com>
2023-11-06 16:47:57 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//Named export individually
+export var a = 10;
+
+let b = "Hello world";
+let c = {
+  name: "Manu",
+  age: 25
+};
+
+//Named export at the bottom
+export { b, c };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { a } from "./app";
+
+console.log(a);
+import { b, c } from "./app";
+
+console.log(b);
+console.log(c.name);
